a20139dd4e8a082f2f00ac4fd7707d29ddfaeda3
meritxe11 <90689445+meritxe11@users.noreply.github.com>
2022-05-18 01:07:46 +0200

    Add files via upload

---
+++ back/new_word.php
@@ -0,0 +1,15 @@
+<?php
+	require_once 'dbconfig.php';
+    $id = rand(1,333);
+	$query = "SELECT `Word` FROM dictionary2 WHERE `Id` = '{$id}'";
+	$result = mysqli_query($connection, $query) or die("Error in Selecting " . mysqli_error($connection));
+
+
+  while($row = mysqli_fetch_assoc($result)) {
+  	$data_to_send[] = $row;
+	}
+
+	echo json_encode([$data_to_send]);
+
+	mysqli_close($connection);
+?>
